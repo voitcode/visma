49bf09934225e84c463c5a01f849244f34582e9a
Runar Ingebrigtsen <runar@rin.no>
2014-12-11 11:38:37 +0100

    belongs_to edi_provider

--- app/models/visma/supplier_edi_profile.rb
+++ app/models/visma/supplier_edi_profile.rb
@@ -3,4 +3,6 @@ class Visma::SupplierEdiProfile < ActiveRecord::Base
   self.table_name = VISMA_CONFIG["table_name_prefix"]
   self.table_name += "SupplierEdiProfile"
   self.primary_key = "SupplierEdiProfileNo"
+
+  belongs_to :edi_provider, primary_key: "EDIProviderNo", foreign_key: "EDIProviderNo"
 end
