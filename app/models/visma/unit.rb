473059a6694ad33a28daa35f538b51127e63c39c
Runar Ingebrigtsen <runar@rin.no>
2015-01-19 23:36:07 +0100

    add Visma::Unit

---
+++ app/models/visma/unit.rb
@@ -0,0 +1,8 @@
+class Visma::Unit < ActiveRecord::Base
+  establish_connection(:visma)
+  self.table_name = VISMA_CONFIG["table_name_prefix"]
+  self.table_name += "Units"
+  self.primary_key = "UnitNo"
+
+  has_many :articles, primary_key: :UnitNo, foreign_key: :UnitNo
+end
