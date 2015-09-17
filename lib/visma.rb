6bb646a6585f9fb603c3dc54d722efbca9757e1f
Runar Ingebrigtsen <runar@rin.no>
2015-09-17 12:18:31 +0200

    include created_scopes

--- lib/visma.rb
+++ lib/visma.rb
@@ -1,6 +1,7 @@
 require "visma/engine"
 require "visma/error"
 require "visma/change_by"
+require "visma/created_scopes"
 require "visma/timestamp"
 require "visma/article_change"
 require "visma/gtin_validator"
