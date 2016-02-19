9f5b0e1acccd75c6b9bf8e68ae4b2ecaec60c4f7
Runar Ingebrigtsen <runar@rin.no>
2016-02-19 14:07:11 +0100

    add to_s to addresses

--- app/models/visma/customer_delivery_addresses.rb
+++ app/models/visma/customer_delivery_addresses.rb
@@ -3,4 +3,21 @@ class Visma::CustomerDeliveryAddresses < ActiveRecord::Base
   self.table_name = VISMA_CONFIG["table_name_prefix"]
   self.table_name += "CustomerDeliveryAddresses"
   self.primary_key = :DeliveryAddressNo
+
+  # Norwegian formatting
+  def to_s
+    lines = [
+      name, line1, line2, line3,
+      "#{zip} #{city}"
+    ].collect {|ln| ln.blank? ? nil : ln }.compact
+    lines.join("\n")
+  end
+
+  # Common names for different address models
+  def name; self.DeliveryName; end
+  def line1; self.DeliveryAddress1; end
+  def line2; self.DeliveryAddress2; end
+  def line3; self.DeliveryAddress3; end
+  def zip; self.DeliveryPostCode; end
+  def city; self.DeliveryPostOffice; end
 end
--- app/models/visma/customer_invoice_adresses.rb
+++ app/models/visma/customer_invoice_adresses.rb
@@ -3,4 +3,21 @@ class Visma::CustomerInvoiceAdresses < ActiveRecord::Base
   self.table_name = VISMA_CONFIG["table_name_prefix"]
   self.table_name += "CustomerInvoiceAdresses"
   self.primary_key = "InvoiceAdressNo"
+
+  # Norwegian formatting
+  def to_s
+    lines = [
+      name, line1, line2, line3,
+      "#{zip} #{city}"
+    ].collect {|ln| ln.blank? ? nil : ln }.compact
+    lines.join("\n")
+  end
+
+  # Common names for different address models
+  def name; self.InvoiceAdressName; end
+  def line1; self.InvoiceAdress1; end
+  def line2; self.InvoiceAdress2; end
+  def line3; self.InvoiceAdress3; end
+  def zip; self.InvoiceAdressPostCode; end
+  def city; self.InvoiceAdressPostoffice; end
 end
