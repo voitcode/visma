cdbb5e5d1c57f2f2543787d98c064d8fb3df59e2
Runar Ingebrigtsen <runar@rin.no>
2016-08-09 15:25:36 +0200

    order status description

--- app/models/visma/customer_order.rb
+++ app/models/visma/customer_order.rb
@@ -13,5 +13,5 @@ class Visma::CustomerOrder < ActiveRecord::Base
   has_many :customer_order_lines, foreign_key: :OrderNo
   alias :order_lines :customer_order_lines
 
-  enum OrderStatus: { invoiced: 1030, nullified: -1, for_picking: 1015, credit_note: 1000 }
+  enum OrderStatus: { system_invoiced: 1030, user_invoiced: 1000, nullified: -1, for_picking: 1015}
 end
