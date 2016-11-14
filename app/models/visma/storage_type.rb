9fd1bb4ffad8277c317e5ebaec0b8d173bf1a717
Viktor Basso <viktor@voit.no>
2016-11-14 15:53:27 +0100

    added primary keys to models so sync will work on bulk api

--- app/models/visma/storage_type.rb
+++ app/models/visma/storage_type.rb
@@ -2,6 +2,7 @@ class Visma::StorageType < ActiveRecord::Base
   establish_connection(:visma)
   self.table_name = VISMA_CONFIG["table_name_prefix"]
   self.table_name += "StorageType"
+  self.primary_key = "StorageTypeNo"
 
   belongs_to :article, foreign_key: :StorageTypeNo, primary_key: :StorageTypeNo
 end
