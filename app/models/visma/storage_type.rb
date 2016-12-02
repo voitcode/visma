accb875a57daa51ab37dc80faa332ce6c4df9c4b
Runar Ingebrigtsen <runar@rin.no>
2016-12-02 15:23:47 +0100

    add timestamp to StorageType

--- app/models/visma/storage_type.rb
+++ app/models/visma/storage_type.rb
@@ -1,6 +1,9 @@
 class Visma::StorageType < Visma::Base
   self.table_name += 'StorageType'
   self.primary_key = 'StorageTypeNo'
+  include Visma::Timestamp
+  include Visma::CreatedScopes
+  include Visma::ChangeBy
 
   belongs_to :article, foreign_key: :StorageTypeNo, primary_key: :StorageTypeNo
 end
