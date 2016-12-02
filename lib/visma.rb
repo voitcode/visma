3a48a617d6d15b785b3caa508c1cbc62e509e116
Runar Ingebrigtsen <runar@rin.no>
2016-12-02 15:40:46 +0100

    order of requirements is important

--- lib/visma.rb
+++ lib/visma.rb
@@ -3,11 +3,11 @@ require "visma/error"
 require "visma/change_by"
 require "visma/created_scopes"
 require "visma/created_by"
-require "visma/full_timestamp"
 require "visma/timestamp"
 require "visma/static_timestamp"
 require "visma/article_change"
 require "visma/gtin_validator"
+require "visma/full_timestamp"
 
 module Visma
 end
