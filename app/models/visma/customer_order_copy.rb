b8522f19fd9f80416b247ba3c47b93c8149bb26e
Runar Ingebrigtsen <runar@rin.no>
2016-12-17 23:22:20 +0100

    clean up some code

--- app/models/visma/customer_order_copy.rb
+++ app/models/visma/customer_order_copy.rb
@@ -1,126 +1,151 @@
-class Visma::CustomerOrderCopy < Visma::Base
-  self.table_name += 'CustomerOrderCopy'
-  self.primary_key = 'OrderCopyNo'
+module Visma
+  # Also called Invoice
+  class CustomerOrderCopy < Visma::Base
+    self.table_name += 'CustomerOrderCopy'
+    self.primary_key = 'OrderCopyNo'
 
-  include Visma::StaticTimestamp
-  include Visma::CreatedBy
+    include Visma::StaticTimestamp
+    include Visma::CreatedBy
 
-  include Visma::CreatedScopes
-  belongs_to :customer, foreign_key: :CustomerNo
-  belongs_to :chain, foreign_key: :ChainNo, primary_key: :CustomerNo, class_name: Visma::Customer
+    include Visma::CreatedScopes
+    belongs_to :customer, foreign_key: :CustomerNo
+    belongs_to :chain,
+               foreign_key: :ChainNo,
+               primary_key: :CustomerNo,
+               class_name: Visma::Customer
 
-  belongs_to :our_reference, foreign_key: :OurRef, primary_key: :Name, class_name: Visma::Employee
+    belongs_to :our_reference,
+               foreign_key: :OurRef,
+               primary_key: :Name,
+               class_name: Visma::Employee
 
-  has_many :customer_order_line_copies, foreign_key: :OrderCopyNo
-  alias order_lines customer_order_line_copies
+    has_many :customer_order_line_copies, foreign_key: :OrderCopyNo
+    alias order_lines customer_order_line_copies
 
-  enum OrderStatus: { system_invoiced: 1030, user_invoiced: 1000, nullified: -1, for_picking: 1015 }
-  scope :invoiced, -> { where(OrderStatus: [1030, 1000]) }
+    enum OrderStatus: {
+      system_invoiced: 1030,
+      user_invoiced: 1000,
+      nullified: -1,
+      for_picking: 1015
+    }
 
-  scope :factoring, -> {
-    where(CustomerProfileNo: VISMA_CONFIG['factoring_customer_profile_number'])
-      .where(FormProfileCustNo: VISMA_CONFIG['factoring_form_profile_number'])
-      .where('FactCustomerNo = CustomerNo')
-  }
+    scope :invoiced, -> { where(OrderStatus: [1030, 1000]) }
 
-  # Form profile: Which papers to use
-  belongs_to :form_profile_customer, foreign_key: :FormProfileCustNo
-  # Print profile: How to send papers
-  belongs_to :print_profile, foreign_key: :PrintProfileNo
-  # EDI profile: How to electronically send order data
-  belongs_to :customer_edi_profile, foreign_key: :EdiProfileNo
-  # Customer profile: How to handle the customer financially
-  belongs_to :customer_profile, foreign_key: :CustomerProfileNo
+    scope :factoring, lambda {
+      where(CustomerProfileNo: VISMA_CONFIG['factoring_customer_profile_number'])
+        .where(FormProfileCustNo: VISMA_CONFIG['factoring_form_profile_number'])
+        .where('FactCustomerNo = CustomerNo')
+    }
 
-  # The email sent by Global if this order was billed by the system
-  def system_generated_invoice_email
-    Visma::MailArchive.where("Subject LIKE '%?%'", self.InvoiceNo)
-  end
+    # Form profile: Which papers to use
+    belongs_to :form_profile_customer, foreign_key: :FormProfileCustNo
+    # Print profile: How to send papers
+    belongs_to :print_profile, foreign_key: :PrintProfileNo
+    # EDI profile: How to electronically send order data
+    belongs_to :customer_edi_profile, foreign_key: :EdiProfileNo
+    # Customer profile: How to handle the customer financially
+    belongs_to :customer_profile, foreign_key: :CustomerProfileNo
 
-  # is this a batch invoice?
-  def is_a_batch_invoice?
-    invoice_batch.count != 1
-  end
+    # The email sent by Global if this order was billed by the system
+    def system_generated_invoice_email
+      Visma::MailArchive.where("Subject LIKE '%?%'", self.InvoiceNo)
+    end
 
-  # return batch invoice order lines
-  def invoice_lines
-    Visma::CustomerOrderLineCopy.where(InvoiceNo: self.InvoiceNo)
-  end
+    # is this a batch invoice?
+    def a_batch_invoice?
+      invoice_batch.count != 1
+    end
+    alias is_a_batch_invoice? a_batch_invoice?
 
-  # return invoice batch orders
-  def invoice_batch
-    self.class.where(InvoiceNo: self.InvoiceNo)
-  end
+    # return batch invoice order lines
+    def invoice_lines
+      Visma::CustomerOrderLineCopy.where(InvoiceNo: self.InvoiceNo)
+    end
 
-  # return sum of TotalAmount
-  def invoice_total
-    invoice_batch.map(&:TotalAmount).sum
-  end
+    # return invoice batch orders
+    def invoice_batch
+      self.class.where(InvoiceNo: self.InvoiceNo)
+    end
 
-  # return sum of TotalGross
-  def invoice_gross
-    invoice_batch.map(&:TotalGross).sum
-  end
+    # return sum of TotalAmount
+    def invoice_total
+      invoice_batch.map(&:TotalAmount).sum
+    end
 
-  # compatibilty method
-  def OrderNo
-    self.OrderCopyNo
-  end
+    # return sum of TotalGross
+    def invoice_gross
+      invoice_batch.map(&:TotalGross).sum
+    end
 
-  # Find the Huldt & Lillevik employee
-  def employee
-    Hlonn::Personer.find_by_name(self.SortName) || Hlonn::Personer.new
-  end
+    # compatibilty method
+    def OrderNo
+      self.OrderCopyNo
+    end
 
-  # Select all employee orders
-  def self.all_employee_orders
-    where(ChainNo: VISMA_CONFIG['employee_chain_number']).where(TypeOfChain: 1)
-                                                         .where(InvoiceDate: DateTime.new(2014)..DateTime.now)
-                                                         .where('TotalGross > 0')
-  end
+    # Find the Huldt & Lillevik employee
+    def employee
+      Hlonn::Personer.find_by_name(self.SortName) || Hlonn::Personer.new
+    end
 
-  # All employee orders, since PrintBatchNo
-  def self.all_employee_orders_after(no)
-    all_employee_orders
-      .where(['PrintBatchNo > ?', no])
-  end
+    # Recalculate TotalAmount
+    def recalculate_total_amount
+      total = invoice_lines.map(&:AmountNotRounded).sum
+      self.TotalAmount = (total * 100).ceil / 100.0
+      rounding = self.TotalGross - total - self.TotalVAT
+      self.RoundingAmount = (rounding * 100).ceil / 100.0
+    end
 
-  # A given employee's orders after PrintBatchNo
-  def self.employee_orders_after(emp, no)
-    all_employee_orders_after(no)
-      .where(CustomerNo: emp)
-  end
+    # Select all employee orders
+    def self.all_employee_orders
+      where(ChainNo: VISMA_CONFIG['employee_chain_number'])
+        .where(TypeOfChain: 1)
+        .where(InvoiceDate: DateTime.new(2014)..DateTime.now)
+        .where('TotalGross > 0')
+    end
 
-  # Last print batch
-  def self.last_print_batch
-    order(:PrintBatchNo).last.PrintBatchNo
-  end
+    # All employee orders, since PrintBatchNo
+    def self.all_employee_orders_after(no)
+      all_employee_orders
+        .where(['PrintBatchNo > ?', no])
+    end
 
-  # All employee orders, between PrintBatchNo
-  def self.all_employee_orders_between(f, t)
-    all_employee_orders
-      .where(['PrintBatchNo >= ?', f])
-      .where(['PrintBatchNo <= ?', t])
-  end
+    # A given employee's orders after PrintBatchNo
+    def self.employee_orders_after(emp, no)
+      all_employee_orders_after(no)
+        .where(CustomerNo: emp)
+    end
 
-  # A given employee's orders between PrintBatchNo
-  def employee_orders_between(emp, f, t)
-    all_employee_orders_between(f, t)
-      .where(CustomerNo: emp)
-  end
+    # Last print batch
+    def self.last_print_batch
+      order(:PrintBatchNo).last.PrintBatchNo
+    end
 
-  # Generate given orders to CSV using selected columns
-  def self.to_csv(orders)
-    CSV.generate do |csv|
-      csv << csv_columns
-      orders.each do |order|
-        csv << order.attributes.values_at(*csv_columns)
+    # All employee orders, between PrintBatchNo
+    def self.all_employee_orders_between(f, t)
+      all_employee_orders
+        .where(['PrintBatchNo >= ?', f])
+        .where(['PrintBatchNo <= ?', t])
+    end
+
+    # A given employee's orders between PrintBatchNo
+    def employee_orders_between(emp, f, t)
+      all_employee_orders_between(f, t)
+        .where(CustomerNo: emp)
+    end
+
+    # Generate given orders to CSV using selected columns
+    def self.to_csv(orders)
+      CSV.generate do |csv|
+        csv << csv_columns
+        orders.each do |order|
+          csv << order.attributes.values_at(*csv_columns)
+        end
       end
     end
-  end
 
-  # Selected columns for CSV and employee_orders
-  def self.csv_columns
-    %w(InvoiceNo InvoiceDate TotalGross CustomerNo SortName PrintBatchNo)
+    # Selected columns for CSV and employee_orders
+    def self.csv_columns
+      %w(InvoiceNo InvoiceDate TotalGross CustomerNo SortName PrintBatchNo)
+    end
   end
 end
--- app/models/visma/customer_order_line_copy.rb
+++ app/models/visma/customer_order_line_copy.rb
@@ -1,44 +1,49 @@
-class Visma::CustomerOrderLineCopy < Visma::Base
-  self.table_name += 'CustomerOrderLineCopy'
-  self.primary_key = :UniqueID
+module Visma
+  # Order lines on Invoices (CustomerOrderCopy)
+  class CustomerOrderLineCopy < Visma::Base
+    self.table_name += 'CustomerOrderLineCopy'
+    self.primary_key = :UniqueID
 
-  include Visma::StaticTimestamp
-  include Visma::CreatedBy
-  include Visma::CreatedScopes
-  belongs_to :customer_order_copy, foreign_key: :OrderCopyNo, primary_key: :OrderCopyNo
-  alias order customer_order_copy
-  belongs_to :article, foreign_key: :ArticleNo
+    include Visma::StaticTimestamp
+    include Visma::CreatedBy
+    include Visma::CreatedScopes
+    belongs_to :customer_order_copy,
+               foreign_key: :OrderCopyNo,
+               primary_key: :OrderCopyNo
+    alias order customer_order_copy
+    belongs_to :article, foreign_key: :ArticleNo
 
-  belongs_to :sub_group, foreign_key: :SubGroupNo
-  belongs_to :main_group, foreign_key: :MainGroupNo
+    belongs_to :sub_group, foreign_key: :SubGroupNo
+    belongs_to :main_group, foreign_key: :MainGroupNo
 
-  has_one :customer, through: :customer_order_copy
+    has_one :customer, through: :customer_order_copy
 
-  # The net price after discount
-  def net_price
-    (self.NetPrice - (self.NetPrice * self.DiscountI / 100)).round(2)
-  rescue
-    0
-  end
+    # The net price after discount
+    def net_price
+      (self.NetPrice - (self.NetPrice * self.DiscountI / 100)).round(2)
+    rescue
+      0
+    end
 
-  # The margin per unit
-  def unit_margin
-    (net_price - self.PurchasePrice).round(2)
-  rescue
-    0
-  end
+    # The margin per unit
+    def unit_margin
+      (net_price - self.PurchasePrice).round(2)
+    rescue
+      0
+    end
 
-  # The net margin for this line
-  def margin
-    (unit_margin * self.Invoiced).round(2)
-  rescue
-    0
-  end
+    # The net margin for this line
+    def margin
+      (unit_margin * self.Invoiced).round(2)
+    rescue
+      0
+    end
 
-  class << self
-    # Find order lines for given customer
-    def for_customer_no(customer_number)
-      joins(:customer).where(customer: { CustomerNo: customer_number })
+    class << self
+      # Find order lines for given customer
+      def for_customer_no(customer_number)
+        joins(:customer).where(customer: { CustomerNo: customer_number })
+      end
     end
   end
 end
