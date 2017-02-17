e3b992001a12e4092bec56945939899db21ede25
Runar Ingebrigtsen <runar@rin.no>
2017-02-17 23:14:50 +0100

    concern missing

--- lib/visma/primary_key.rb
+++ lib/visma/primary_key.rb
@@ -2,7 +2,9 @@ module Visma
   # Set and validate the primary key before saving a record
   module PrimaryKey
     included do
+      extend ActiveSupport::Concern
       validates primary_key, presence: true
+      before_create :generate_primary_key
     end
   end
 end
