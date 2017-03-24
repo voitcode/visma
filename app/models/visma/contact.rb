12fb28bc96db4cf8737a874d1796454a667f26f0
Runar Ingebrigtsen <runar@rin.no>
2017-03-24 21:33:17 +0100

    contacts timestamp

--- app/models/visma/contact.rb
+++ app/models/visma/contact.rb
@@ -1,6 +1,10 @@
 class Visma::Contact < Visma::Base
   self.table_name += 'Contact'
   self.primary_key = 'ContactNo'
+  include Visma::Timestamp
+  include Visma::CreatedScopes
+  include Visma::ChangeBy
+
   enum InActiveYesNo: [:active, :inactive]
 
   belongs_to :customer, foreign_key: :CustomerNo
