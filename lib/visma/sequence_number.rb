e2556be325ef8f389ce946d319fd9ff96325f205
Runar Ingebrigtsen <runar@rin.no>
2017-05-03 14:23:28 +0200

    fix sequence without siblings

--- lib/visma/sequence_number.rb
+++ lib/visma/sequence_number.rb
@@ -7,15 +7,19 @@ module Visma
 
     # Set the sequence number to the next in range
     def set_sequence
-      seq = sequence.last + 1
+      seq = next_sequence
       self.SeqNo = format('%08d', seq.to_s.reverse.to_i).reverse
     end
 
     # Define the sequence numbering for all siblings
     def sequence
       siblings.pluck(:SeqNo).collect { |n| n.to_s.sub(/0+$/, '').to_i }.sort
+    end
+
+    def next_sequence
+      sequence.last + 1
     rescue NoMethodError
-      [1000]
+      1001
     end
   end
 end
--- lib/visma/version.rb
+++ lib/visma/version.rb
@@ -1,3 +1,3 @@
 module Visma
-  VERSION = '0.5.47'.freeze
+  VERSION = '0.5.48'.freeze
 end
