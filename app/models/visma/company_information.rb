09e8c535edd2be44ae194f0f7543d8a0e40f1275
Runar Ingebrigtsen <runar@rin.no>
2017-06-14 14:45:13 +0200

    add BankConnection

--- app/models/visma/company_information.rb
+++ app/models/visma/company_information.rb
@@ -1,4 +1,9 @@
-class Visma::CompanyInformation < Visma::Base
-  self.table_name += 'CompanyInformation'
-  self.primary_key = 'CompanyNo'
+module Visma
+  # Information about the licensed company using Visma Global
+  class CompanyInformation < Visma::Base
+    self.table_name += 'CompanyInformation'
+    self.primary_key = 'CompanyNo'
+
+    belongs_to :bank_connection, foreign_key: :BankConNo
+  end
 end
