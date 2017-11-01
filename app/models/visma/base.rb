4a9b4c11afc1f7cfa2d7a25f5f44869c442319f8
Runar Ingebrigtsen <runar@rin.no>
2017-11-01 15:26:55 +0100

    use environment specific visma database

--- app/models/visma/base.rb
+++ app/models/visma/base.rb
@@ -1,7 +1,7 @@
 module Visma
   # Common methods for Visma models
   class Base < ActiveRecord::Base
-    establish_connection(:visma)
+    establish_connection("visma_#{Rails.env}")
     self.table_name = VISMA_CONFIG['table_name_prefix']
   end
 end
--- lib/visma/version.rb
+++ lib/visma/version.rb
@@ -1,3 +1,3 @@
 module Visma
-  VERSION = '0.5.69'.freeze
+  VERSION = '0.5.70'.freeze
 end
