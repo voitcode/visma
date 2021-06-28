aabb88b5482f70c488612eee0ba72b20f42dbd9a
Runar Ingebrigtsen <runar@rin.no>
2021-06-28 16:02:33 +0200

    include module

--- lib/visma.rb
+++ lib/visma.rb
@@ -10,6 +10,7 @@ require 'visma/gtin_validator'
 require 'visma/primary_key'
 require 'visma/sequence_number'
 require 'visma/discount_party_validator'
+require 'visma/polymorphic_name'
 
 # This is the Visma Global ERP integration module
 module Visma
